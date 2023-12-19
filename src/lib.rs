7706a41840c3ecec2d398789d8d78e81cb2395d9
Josh <josh@grayd.us>
2023-12-19 23:48:37 +0900

    refactor, cleanup, add comments

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ use nalgebra::{
 };
 use std::{
   cell::RefCell,
-  collections::HashSet,
   rc::Rc,
 };
 use wasm_bindgen::prelude::*;
@@ -18,6 +17,7 @@ use web_sys::{
 };
 
 // make printing a console log more convenient
+#[allow(unused)]
 macro_rules! log {
   ( $( $t:tt )* ) => {
     web_sys::console::log_1(&format!( $( $t )* ).into());
@@ -33,8 +33,6 @@ use model::*;
 mod shader;
 use shader::*;
 
-const SIZE: u32 = 600;
-
 fn get_canvas() -> web_sys::HtmlCanvasElement {
   let window = web_sys::window().unwrap();
   let document = window.document().unwrap();
@@ -43,8 +41,6 @@ fn get_canvas() -> web_sys::HtmlCanvasElement {
       .unwrap()
       .dyn_into::<web_sys::HtmlCanvasElement>()
       .unwrap();
-  canvas.set_height(SIZE);
-  canvas.set_width(SIZE);
   canvas
 }
 
@@ -61,49 +57,63 @@ fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     .request_animation_frame(f.as_ref().unchecked_ref()).unwrap();
 }
 
+// WARNING: currently supports a maximum of 255 vertices because
+// indices are represented with u8 values.
 const NUM_VERTICES: i32 = 200;
 
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
+    let canvas = get_canvas();
+    let aspect_ratio = canvas.width() as f32 / canvas.height() as f32;
+
     wasm_bindgen_futures::spawn_local(async move {
+      // the mouse handlers let the user click/drag to rotate the model
       let angles = set_up_mouse_handlers();
 
+      // initialize the geometry
+      let model = Rc::new(RefCell::new(Model::new(NUM_VERTICES)));
+      model.borrow_mut().update_distance_threshold(0.15);
+
+      // webgl prep
       let ctx = get_webgl_context();
       let program = make_shader_program(&ctx);
-
-      let distance_threshold: f32 = 0.25;
-      let mut model = Rc::new(RefCell::new(Model::new(NUM_VERTICES)));
-      model.borrow_mut().update_distance_threshold(distance_threshold);
-
       model.borrow().load_vertex_data(&ctx, &program);
       model.borrow().load_index_data(&ctx);
 
+      // listen for left/right arrow keys to change the distance threshold
       set_up_keypress_handler(ctx.clone(), model.clone());
 
-      let eye = Point3::new(0.0, 0.0, 1.0);
+      // set up the camera
+      let eye = Point3::new(0.0, 0.0, 0.75);
       let target = Point3::new(0.0, 0.0, 0.0);
       let view = Isometry3::look_at_rh(&eye, &target, &Vector3::y());
-      let projection = Perspective3::new(1.0, 3.14 / 2.0, 0.0, 1000.0);
+      let projection = Perspective3::new(aspect_ratio, 3.14 / 2.0, 0.0, 1000.0);
       let view_projection = projection.into_inner() * view.to_homogeneous();
 
-      let draw_fn0= Rc::new(RefCell::new(None));
+      // the draw function needs to be a closure that references itself. this
+      // is a bit tricky to do in rust. the technique used here is to put the
+      // closure in a RefCell within an Rc. the closure can reference itself
+      // via the captured Rc. additionally, a copy of the Rc is made in order
+      // to schedule the first draw
+      let draw_fn0 = Rc::new(RefCell::new(None));
       let draw_fn1 = draw_fn0.clone();
 
       *draw_fn1.borrow_mut() = Some(Closure::new(move || {
+        // update the transform matrix with the current rotation angles
         let (a, b) = *angles.borrow();
-
         let model_view_projection = {
           let model = Matrix4::from_euler_angles(b, a, 0.0);
           view_projection * model
         };
-
+        // draw
         model.borrow().draw(&ctx, &program, model_view_projection.as_slice());
-
+        // schedule the next draw
         request_animation_frame(draw_fn0.borrow().as_ref().unwrap());
       }));
 
+      // schedule the first draw
       request_animation_frame(draw_fn1.borrow().as_ref().unwrap());
     });
 
--- src/model.rs
+++ src/model.rs
@@ -1,28 +1,35 @@
 use std::collections::HashSet;
 use web_sys::{
   WebGl2RenderingContext,
-  WebGlProgram
+  WebGlProgram,
+  WebGlUniformLocation,
 };
 
 fn generate_vertices(n: i32) -> Vec<(f32,f32,f32)> {
+  // random in range (-0.5, 0.5)
   fn rnd() -> f32 {
     let r: f32 = rand::random();
     r - 0.5f32
   }
+  // generate n points within a sphere of radius 0.5
   std::iter::from_fn(|| { Some((rnd(), rnd(), rnd())) })
-    .filter(|(x,y,z)| x*x + y*y + z*z < 1.0)
+    .filter(|(x,y,z)| x*x + y*y + z*z < 0.25)
     .take(n as usize)
     .collect()
 }
 
 pub struct Model {
-  vertices: Vec<(f32,f32,f32)>,
-  lines: Vec<(u8,u8)>,
-  triangles: Vec<(u8,u8,u8)>,
+  vertices: Vec<(f32,f32,f32)>, // 3D points
+  lines: Vec<(u8,u8)>,          // vertex indices
+  triangles: Vec<(u8,u8,u8)>,   // vertex indices
   distance_threshold: f32,
 }
 
 impl Model {
+  const POINT_COLOR: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+  const LINE_COLOR: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+  const TRIANGLE_COLOR: [f32; 4] = [0.3, 0.0, 0.0, 1.0];
+
   pub fn new(num_vertices: i32) -> Self {
     Self {
       vertices: generate_vertices(num_vertices),
@@ -32,9 +39,6 @@ impl Model {
     }
   }
 
-  pub fn vertices(&self) -> &Vec<(f32,f32,f32)> { &self.vertices }
-  pub fn lines(&self) -> &Vec<(u8,u8)> { &self.lines }
-  pub fn triangles(&self) -> &Vec<(u8,u8,u8)> { &self.triangles }
   pub fn distance_threshold(&self) -> f32 { self.distance_threshold }
 
   fn distance(&self, i: usize, j: usize) -> f32 {
@@ -46,10 +50,13 @@ impl Model {
     (x*x + y*y + z*z).sqrt()
   }
 
+  // recompute the lines and triangles based on the new distance threshold
   pub fn update_distance_threshold(&mut self, distance_threshold: f32) {
     self.lines = {
       let mut result = Vec::new();
       for i in 0..self.vertices.len() {
+        // (i,j) and (j,i) are the same edge, so to prevent
+        // duplicates only generate the edges where i < j
         for j in i+1..self.vertices.len() {
           if self.distance(i, j) < distance_threshold {
             result.push((i as u8,j as u8));
@@ -60,11 +67,12 @@ impl Model {
     };
     self.triangles = {
       let lines_set: HashSet<(u8,u8)> = self.lines.iter().map(|x| *x).collect();
+      // disregard vertices that aren't in any of the lines
       let vertices: HashSet<u8> = self.lines.iter().flat_map(|(a,b)| vec![*a,*b]).collect();
       let mut result: HashSet<(u8,u8,u8)> = HashSet::new();
       for &(a,b) in &self.lines {
         for &v in vertices.iter() {
-          if v > b {
+          if v > b { // prevents duplicates by enforcing a < b < v
             let l1 = (a,v);
             let l2 = (b,v);
             if lines_set.contains(&l1) && lines_set.contains(&l2) {
@@ -113,6 +121,7 @@ impl Model {
     &self,
     ctx: &WebGl2RenderingContext,
   ) {
+    // flatten the lines/triangles and concatenate them into a single Vec<u8>
     let mut lines: Vec<u8> = self.lines.iter().flat_map(|(i,j)| { vec![*i, *j] }).collect();
     let mut triangles: Vec<u8> = self.triangles.iter().flat_map(|(i,j,k)| vec![*i,*j,*k]).collect();
     let mut geometry = Vec::new();
@@ -128,10 +137,15 @@ impl Model {
     );
   }
 
-  pub fn draw_vertices(
+  fn draw_vertices(
     &self,
     ctx: &WebGl2RenderingContext,
+    color_location: &Option<WebGlUniformLocation>,
   ) {
+    ctx.uniform4fv_with_f32_array(
+      color_location.as_ref(),
+      &Self::POINT_COLOR,
+    );
     ctx.draw_arrays(
       WebGl2RenderingContext::POINTS,
       0,
@@ -139,26 +153,37 @@ impl Model {
     );
   }
 
-  pub fn draw_lines(
+  fn draw_lines(
     &self,
     ctx: &WebGl2RenderingContext,
+    color_location: &Option<WebGlUniformLocation>,
   ) {
+    ctx.uniform4fv_with_f32_array(
+      color_location.as_ref(),
+      &Self::LINE_COLOR,
+    );
     ctx.draw_elements_with_i32(
       WebGl2RenderingContext::LINES,
-      self.lines().len() as i32 * 2,
+      self.lines.len() as i32 * 2,
       WebGl2RenderingContext::UNSIGNED_BYTE,
       0,
     );
   }
 
-  pub fn draw_triangles(
+  fn draw_triangles(
     &self,
     ctx: &WebGl2RenderingContext,
+    color_location: &Option<WebGlUniformLocation>,
   ) {
+    ctx.uniform4fv_with_f32_array(
+      color_location.as_ref(),
+      &Self::TRIANGLE_COLOR,
+    );
     ctx.draw_elements_with_i32(
       WebGl2RenderingContext::TRIANGLES,
       self.triangles.len() as i32 * 3,
       WebGl2RenderingContext::UNSIGNED_BYTE,
+      // offset by the size of the line data which is at the beginning of the buffer
       self.lines.len() as i32 * 2,
     );
   }
@@ -169,34 +194,18 @@ impl Model {
     program: &WebGlProgram,
     model_view_projection: &[f32],
   ) {
-    let color_location = ctx.get_uniform_location(program, "color");
-    let color1: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-    let color2: [f32; 4] = [0.4, 0.0, 0.0, 1.0];
-
     let transform_location = ctx.get_uniform_location(program, "transform");
-
     ctx.uniform_matrix4fv_with_f32_array(
       transform_location.as_ref(),
       false,
       model_view_projection,
     );
 
+    let color_location = ctx.get_uniform_location(program, "color");
     ctx.clear_color(0.0, 0.0, 0.0, 1.0);
     ctx.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
-
-    ctx.uniform4fv_with_f32_array(
-      color_location.as_ref(),
-      &color2,
-    );
-
-    self.draw_triangles(&ctx);
-
-    ctx.uniform4fv_with_f32_array(
-      color_location.as_ref(),
-      &color1,
-    );
-
-    self.draw_vertices(&ctx);
-    self.draw_lines(&ctx);
+    self.draw_triangles(&ctx, &color_location);
+    self.draw_vertices(&ctx, &color_location);
+    self.draw_lines(&ctx, &color_location);
   }
 }
